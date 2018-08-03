488ea21653af2929fab4ead3faa5d1b46665ffc6
ezaga <paulezaga@gmail.com>
2018-08-03 16:45:56 +0200

    Implemented Angular-In-Memory API

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,21 +3,29 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heros';
 import { Observable, of} from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient ) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService:fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
