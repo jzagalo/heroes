b2bd9a11dc386460d4f8238a763bb2b53b5ad533
ezaga <paulezaga@gmail.com>
2018-08-03 11:51:24 +0200

    Added Routing to Test Project

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,17 +14,17 @@ export class HeroesComponent implements OnInit {
 
   constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
-    this.getHeroes();
-  }
+    ngOnInit() {
+      this.getHeroes();
+    }
 
-  getHeroes(): void {
-     this.heroService.getHeroes()
-       .subscribe(heroes => this.heroes = heroes);
-  }
+    getHeroes(): void {
+       this.heroService.getHeroes()
+         .subscribe(heroes => this.heroes = heroes);
+    }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
 
 }
